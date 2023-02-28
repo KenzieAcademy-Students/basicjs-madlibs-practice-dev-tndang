9fb279e2bdb8eb2a24962b3c26b92bc31d902570
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-02-28 11:29:55 -0500

    Created my own MadLibs Story

--- code.js
+++ code.js
@@ -3,17 +3,24 @@
 
 // Create your own Madlibs story!
 
-let animal = prompt("Name a kind of animal (singular):");
-let place = prompt("Name a place:");
-let food = prompt("Name a food (plural):");
+let adjective = prompt("Enter an adjective");
+let singWeapon = prompt("Enter a weapon (singular)");
+let ingVerb = prompt("Enter a verb ending with -ing")
+let item = prompt("Enter an item needed for survival (plural)")
+let food = prompt("Name a food that could fit easily in the hand (plural)")
 
 document.write("Here is a story for you!<br>");
 document.write(
-  "The giant "
-  + animal
-  + " walked proudly through "
-  + place
-  + ", dropping "
+  "The "
+  + adjective
+  + " adventurer entered a random shop looking for a "
+  + singWeapon
+  + " of "
+  + ingVerb
+  + ". "
+  + "Inside this shop were some racks of old dusty "
+  + item
+  + " and "
   + food
-  + " everywhere."
+  + " that look like they were well past a reasonable condition to consume."
 );
